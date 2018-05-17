057f712763fde1861be7e716b876db2db17fe2ea
Valerio Marcelli <marcelli@seattleu.edu>
2018-05-17 12:13:43 -0700

    Added Admin's Student and Expanded Student Pages
    Very basic html pages added, only the title has been included for the pages so far

---
+++ 5051/Controllers/AdminExpandedStudent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _5051.Controllers
+{
+    public class AdminExpandedStudentController : Controller
+    {
+        // GET: AdminExpandedStudent
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 5051/Controllers/AdminStudentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _5051.Controllers
+{
+    public class AdminStudentController : Controller
+    {
+        // GET: AdminStudent
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
