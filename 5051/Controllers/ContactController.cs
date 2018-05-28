d4fc6bdddaf191ab8b646bdaee84a832b30e6e04
aguiarj1 <aguiarj1@seattleu.edu>
2018-05-27 23:22:37 -0700

    trying to add post with a contactinfo class

---
+++ 5051/Controllers/ContactController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _5051.Models;
+using _5051.Backend;
+
+namespace _5051.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            var contact = new ContactInfo() { Id=1, Name = "Peter Pan", Message="This is a message" }; 
+            return View(contact);
+        }
+
+        public ActionResult Update([Bind(Include =
+                                        "Id,"+
+                                        "Name,"+
+                                        "Message,"+
+                                        "")] ContactInfo data)
+        {
+           
+            return RedirectToAction("Index"); 
+        }
+    }
+}
---
+++ 5051/Models/ContactInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _5051.Models
+{
+    public class ContactInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Message { get; set; }
+    }
+}
