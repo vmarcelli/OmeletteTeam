dd594547d8755e6dea37d09c93c94a5b1986a311
Vilia Wang <wangv2@seattleu.edu>
2018-06-01 09:24:22 -0700

    added comments to admin login page

--- 5051/Controllers/AdminLoginController.cs
+++ 5051/Controllers/AdminLoginController.cs
@@ -6,6 +6,9 @@ using System.Web.Mvc;
 
 namespace _5051.Controllers
 {
+    /// <summary>
+    /// The AdminLoginController manages the Administrator login page 
+    /// </summary>
     public class AdminLoginController : Controller
     {
         // GET: Admin
