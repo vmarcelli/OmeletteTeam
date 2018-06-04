b73b6d3087b148259043b8fab236719daf5df812
Jack Joseph <josephj4@seattleu.edu>
2018-06-03 21:36:12 -0700

    updated kiosk index view & js file documentation

--- 5051/Scripts/kiosk.js
+++ 5051/Scripts/kiosk.js
@@ -1,16 +1,18 @@
 ﻿// javascript for kiosk page
 
 window.onload = function () {
+    // initial image opacity
     var opacity = 0.4;
 
-    // gets student kiosk box
+    // gets student elements in an array
     var students = document.getElementsByClassName("student");
 
+    // sets student image opacity as 'opacity' to start, then adds sign in/ out click function
     for (var i = 0; i < students.length; i++) {
         students[i].firstElementChild.style.opacity = opacity;
 
         students[i].onclick = function () {
-            // sets opacity of photo and displays correct signin/signout message
+            // alternates brighten/ darken on click of student box
             if (this.firstElementChild.style.opacity == opacity) {
                 this.firstElementChild.style.opacity = 1;
             } else {
