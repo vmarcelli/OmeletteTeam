2d151cde44cba22554020a24e69cbb2fc356fb9a
Jack Joseph <josephj4@seattleu.edu>
2018-06-03 21:30:52 -0700

    Added signin/signout opacity changes in kiosk

--- 5051/Scripts/kiosk.js
+++ 5051/Scripts/kiosk.js
@@ -1,55 +1,24 @@
 ﻿// javascript for kiosk page
 
 window.onload = function () {
-/*
+    var opacity = 0.4;
 
     // gets student kiosk box
-    var students = document.getElementsByClassName("box");
+    var students = document.getElementsByClassName("student");
 
     for (var i = 0; i < students.length; i++) {
+        students[i].firstElementChild.style.opacity = opacity;
+
         students[i].onclick = function () {
-            // gets signin/signout modal
-            var signin = document.getElementById('in');
-            var signout = document.getElementById('out');
-
-            // gets signin/ signout close buttons
-            var inSpan = document.getElementsByClassName("close")[0];
-            var outSpan = document.getElementsByClassName("close")[1];
-
-            // gets the photo element of the student box
-            var photo = this.firstElementChild;
-
             // sets opacity of photo and displays correct signin/signout message
-            if (photo.style.opacity == 0.5) {
-                photo.style.opacity = 1;
-                signin.style.display = "block";
+            if (this.firstElementChild.style.opacity == opacity) {
+                this.firstElementChild.style.opacity = 1;
             } else {
-                photo.style.opacity = 0.5;
-                signout.style.display = "block";
-            }
-
-            // closes when user clicks the signin or signout 'x'
-            inSpan.onclick = function () {
-                signin.style.display = "none";
-            }
-
-            outSpan.onclick = function () {
-                signout.style.display = "none";
-            }
-
-
-
-            // closes modal if user clicks anywhere outside of modal
-            window.onclick = function (event) {
-                if (event.target == signin) {
-                    signin.style.display = "none";
-                } else if (event.target == signout) {
-                    signout.style.display = "none";
-                }
+                this.firstElementChild.style.opacity = opacity;
             }
         } 
     }
-    */
+    
 };
 
 
