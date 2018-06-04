d6c5e8ff80f52094e384e7d86d3b6ffc144d1e08
Jack Joseph <josephj4@seattleu.edu>
2018-06-03 22:26:39 -0700

    Added rough greeting and farewell on sign in/ out in kiosk

--- 5051/Scripts/kiosk.js
+++ 5051/Scripts/kiosk.js
@@ -4,23 +4,52 @@ window.onload = function () {
     // initial image opacity
     var opacity = 0.4;
 
+    var greetingNode = 1;
+    var farewellNode = 3;
+    var imgNode = 5;
+
+
     // gets student elements in an array
     var students = document.getElementsByClassName("student");
 
     // sets student image opacity as 'opacity' to start, then adds sign in/ out click function
     for (var i = 0; i < students.length; i++) {
-        students[i].firstElementChild.style.opacity = opacity;
+        students[i].childNodes[imgNode].style.opacity = opacity;
 
         students[i].onclick = function () {
             // alternates brighten/ darken on click of student box
-            if (this.firstElementChild.style.opacity == opacity) {
-                this.firstElementChild.style.opacity = 1;
+            if (this.childNodes[imgNode].style.opacity == opacity) {
+                this.childNodes[imgNode].style.opacity = 1;
+                showGreeting(this);
             } else {
-                this.firstElementChild.style.opacity = opacity;
+                this.childNodes[imgNode].style.opacity = opacity;
+                showFarewell(this);
             }
         } 
     }
+
+    function showGreeting(student) {
+        student.childNodes[farewellNode].style.display = "none";
+        student.childNodes[greetingNode].style.display = "block";
+
+        setTimeout(
+            function () {
+                student.childNodes[greetingNode].style.display = "none";
+            }, 1300);
+    };
+
+    function showFarewell(student) {
+        student.childNodes[greetingNode].style.display = "none";
+        student.childNodes[farewellNode].style.display = "block";
+
+        setTimeout(
+            function () {
+                student.childNodes[farewellNode].style.display = "none";
+            }, 1300);
+    };
     
 };
+
+
 
 
